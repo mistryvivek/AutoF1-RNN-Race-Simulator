530a197ffc1b0b440f40a80408c7126386958d8a
Vivek Mistry <vm800@york.ac.uk>
2025-01-05 13:56:08 +0000

    merged all fastf1 dataframes

--- Data Gathering/1_create_local_dataset_from_api.py
+++ Data Gathering/1_create_local_dataset_from_api.py
@@ -38,26 +38,53 @@ def main():
             # Merge the telemetry data.
             combined_dataset = session.laps.reset_index(drop=True)
             
+            # Add columns we are expecting for telemetry data.
+            telemetry_data_columns = [
+                'Speed', 'RPM', 'nGear', 'Throttle', 'Brake', 'DRS',  # Car data
+                'X', 'Y', 'Z', 'Status',  # Position data
+                'DriverAhead', 'DistanceToDriverAhead' # Compare position
+            ]
+
+            combined_dataset[telemetry_data_columns] = pd.NA
+
+            combined_dataset['MandatoryPitStop'] = True
+            # Mandatory pit stop made - is not ready here either.
+            if session == "Race":
+                for driver in combined_dataset["Driver"].unique():
+                    driversRace = combined_dataset[combined_dataset["Driver"] == driver].sort_values("LapNumber")
+                    for idx, lap in driversRace:
+                        if (lap["Compound"] == "WET" or lap["Compound"] == "INTERMEDIATE") or \
+                           (lap["Compound"] != driversRace.iloc[0]["Compound"]):
+                            break
+                        else:
+                            lap.iloc[idx, "Compound"] = False
+                combined_dataset.to_csv("test.csv")
+                exit()
+
             for idx, lap in combined_dataset.iterrows():
                 # There are so many telemetry points - we are looking lap by lap so we just take the last one.
                 telemetry_data = lap.get_telemetry().add_driver_ahead()
-                combined_dataset = combined_dataset.reindex(columns=telemetry_data.columns)
-                combined_dataset.iloc[idx] = telemetry_data.iloc[-1]
-                combined_dataset.to_csv("test.csv")
-            
+                telemetry_data = telemetry_data[telemetry_data_columns]
+                combined_dataset.loc[idx, telemetry_data_columns] = telemetry_data.iloc[-1]
+                break
+
             weather_data = session.laps.get_weather_data().reset_index(drop=True)
             combined_dataset = pd.concat([combined_dataset, weather_data.loc[:, ~(weather_data.columns == 'Time')]], axis=1)
 
+            # Only distance not built in is distance behind - but we can factor that in.     
+                
+            # Did they complete full race distance.
 
-            # Only distance not built in is distance behind - but we can factor that in.
-            # Mandatory pit stop made - is not ready here either
+            # Fields to check what race and session it is.
+            combined_dataset['Country'] = row['Country']
+            combined_dataset['Location'] = row['Location']
+            combined_dataset['OfficialEventName'] = row['OfficialEventName']
+            combined_dataset['EventDate'] = row['EventDate']
+            combined_dataset['EventName'] = row['EventName']
+            combined_dataset['Session'] = session_column
+            combined_dataset['Year'] = year
 
             combined_dataset.to_csv("test.csv")
-            break
-
-
-                
-
-
+   
 if __name__ == "__main__":
     main()
