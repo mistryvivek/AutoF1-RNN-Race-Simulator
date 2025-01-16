314b5678f088bd9870ce5feda2e5aeb56ba067d0
Vivek Mistry <vm800@york.ac.uk>
2025-01-16 13:54:34 +0000

    added new python files

--- Training/Models/model_1_rnn_ff.py
+++ Training/Models/model_1_rnn_ff.py
@@ -1,21 +1,34 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.utils.data import random_split
+from ..f1_dataset import CustomF1Dataloader
+
+HIDDEN_SIZE = 5
+LR = 0.0001
+ITERATIONS = 10000
+INPUT_SIZE = 2
+NUM_CLASSES = 5
+OPTIM = torch.optim.Adagrad
+
+DATASET = CustomF1Dataloader(1, "TyreLife,Compound", "../../Data Gathering")
+TRA
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class UnifiedModelRNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes):
         super(UnifiedModelRNN, self).__init__()
         
-        self.rnn = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
         
         self.fc_pit = nn.Sequential(
-            nn.Linear(hidden_dim, 64),
+            nn.Linear(hidden_size, 64),
             nn.ReLU(),
             nn.Linear(64, num_classes)
         )
         
         self.fc_time = nn.Sequential(
-            nn.Linear(hidden_dim, 64),
+            nn.Linear(hidden_size, 64),
             nn.ReLU(),
             nn.Linear(64, 1)
         )
@@ -28,3 +41,17 @@ class UnifiedModelRNN(nn.Module):
         time_output = self.fc_time(h_n)
         
         return pit_output, time_output
+    
+def train():
+    model = UnifiedModelRNN(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES)
+    model.to(device)
+
+    optim = OPTIM(model.parameters(), lr=LR)
+
+    train_dataset, testing_dataset, validation_dataset = random_split(DATASET, [0.8, 0.1, 0.1])
+
+    print(train_dataset.__getitem__(0))
+    print(testing_dataset.__getitem__(0))
+    print(validation_dataset.__getitem__(0))
+
+train()
